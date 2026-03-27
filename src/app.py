1df58bb2739e36e9afadff7afdd96cb54e5eeca6
technodot <techdudie@mail.com>
2026-03-27 13:56:21 -0500

    feat: style

--- src/app.py
+++ src/app.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
 from fastapi import Cookie, FastAPI, Form, HTTPException as FastAPIHTTPException, Request
-from fastapi.responses import JSONResponse, FileResponse, RedirectResponse, Response
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse, FileResponse, RedirectResponse
 import hashlib
 import jwt
 import math
 from pydantic import BaseModel
-from pyotp import random_base32 as baller, TOTP as authy
+from pyotp import random_base32 as baller, TOTP as nukeynukey
 import sqlite3
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing import Optional
@@ -14,29 +13,15 @@ from typing import Optional
 _ = math.factorial
 technodot = 1/_(6)*_(7)
 
-def V():
-    return [521.0 / technodot for _ in range(7)]
+V = lambda: [521.0 / technodot for _ in range(int(technodot))]
+I = lambda: [2.0 * -6.058310519045666 * math.cos(math.tau * n / technodot) / technodot for n in range(int(technodot))]
+C = lambda: [2.0 * 0.05630403702319953 * math.sin(math.tau * n / technodot) / technodot for n in range(int(technodot))]
+T = lambda: [2.0 * -1.979426188898735 * math.cos(2.0 * math.tau * n / technodot) / technodot for n in range(int(technodot))]
+U = lambda: [2.0 * -30.029206786297053 * math.sin(2.0 * math.tau * n / technodot) / technodot for n in range(int(technodot))]
+N = lambda: [2.0 * -21.462263292055802 * math.cos(3.0 * math.tau * n / technodot) / technodot for n in range(int(technodot))]
+A = lambda: [2.0 * 18.97349650542115 * math.sin(3.0 * math.tau * n / technodot) / technodot for n in range(int(technodot))]
 
-def I():
-    return [2.0 * -6.058310519045666 * math.cos(math.tau * n / technodot) / technodot for n in range(7)]
-
-def C():
-    return [2.0 * 0.05630403702319953 * math.sin(math.tau * n / technodot) / technodot for n in range(7)]
-
-def T():
-    return [2.0 * -1.979426188898735 * math.cos(2.0 * math.tau * n / technodot) / technodot for n in range(7)]
-
-def U():
-    return [2.0 * -30.029206786297053 * math.sin(2.0 * math.tau * n / technodot) / technodot for n in range(7)]
-
-def N():
-    return [2.0 * -21.462263292055802 * math.cos(3.0 * math.tau * n / technodot) / technodot for n in range(7)]
-
-def A():
-    return [2.0 * 18.97349650542115 * math.sin(3.0 * math.tau * n / technodot) / technodot for n in range(7)]
-
-def roller():
-    return "".join(chr(int(round(sum(v[i] for v in (V(), I(), C(), T(), U(), N(), A()))))) for i in range(7))
+roller = lambda role: sum((int(round(sum(v[i] for v in (V(), I(), C(), T(), U(), N(), A())))) - ord(role[i]) for i in range(int(technodot)))) != 0
 
 def dber() -> sqlite3.Connection:
     return sqlite3.connect("default.db")
@@ -133,7 +118,7 @@ async def _login_submit(
         {
             "username": username,
             "role": row[2],
-            "exp": int((datetime.now() + timedelta(seconds=67)).timestamp())
+            "exp": int((datetime.now() + timedelta(minutes=6, seconds=7)).timestamp())
         }, ball, algorithm="HS256"))
     return response
 
@@ -177,14 +162,31 @@ async def _launch_ai_powered_nukes(
     role: Optional[str] = Cookie(None),
     auth: auther = None
 ):
+    if not token:
+        raise FastAPIHTTPException(status_code=400, detail="who is this kid")
+    cursor = dber().cursor()
+    cursor.execute("SELECT value FROM settings WHERE key = 'COMMON_BALL'")
+    try:
+        payload = jwt.decode(token, cursor.fetchone()[0], algorithms=["HS256"]) if token else {}
+    except jwt.ExpiredSignatureError:
+        raise FastAPIHTTPException(status_code=403, detail="who is this kid")
+    except Exception:
+        raise FastAPIHTTPException(status_code=401, detail="who is this kid")
+
+    if payload.get("role") == "user":
+        raise FastAPIHTTPException(status_code=403, detail="what is this kid doing")
+
     if not auth:
         raise FastAPIHTTPException(status_code=400, detail="woah there buddy ts aint cutting it")
     
     cursor = dber().cursor()
     cursor.execute("SELECT value FROM settings WHERE key = 'COMMON_BALL'")
-    if not authy(cursor.fetchone()[0]).verify(auth.code):
+    if not nukeynukey(cursor.fetchone()[0]).verify(auth.code):
         raise FastAPIHTTPException(status_code=403, detail="nuh uh youre getting slimed out\nts code was NOT it")
 
-    if role != roller():
+    try:
+        if roller(role):
+            raise FastAPIHTTPException(status_code=403, detail="we can't be launching AI-POWERED nukes just like that, yk?")
+    except IndexError:
         raise FastAPIHTTPException(status_code=403, detail="we can't be launching AI-POWERED nukes just like that, yk?")
     return JSONResponse(content={"message": "nukes launched successfully!\nwe're all dead! happy now?\nihms{mUTu@llY_@$$Ur3d_D3$$tRuC+10n}"}, status_code=200)
